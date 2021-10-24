3ff57654425bb575d0172c9e3a780ac5d16f0eab
Luiz Rogerio <lrbengozi@gmail.com>
2021-10-24 12:07:41 -0300

    Create styles

--- src/components/MovieRow/index.tsx
+++ src/components/MovieRow/index.tsx
@@ -8,16 +8,20 @@ interface MovieRowProps {
 
 const MovieRow: React.ElementType<MovieRowProps> = ({ title, items }) => {
   return (
-    <div>
+    <div className="movieRow">
       <h2>{title}</h2>
       <div className="movieRow--listarea">
-        {items.results.length > 0 &&
-          items.results.map((item: any) => (
-            <img
-              src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
-              alt={item.title}
-            />
-          ))}
+        <div className="movieRow--list">
+          {items.results.length > 0 &&
+            items.results.map((item: any, key: any) => (
+              <div key={key} className="movieRow--item">
+                <img
+                  src={`https://image.tmdb.org/t/p/w300${item.poster_path}`}
+                  alt={item.title}
+                />
+              </div>
+            ))}
+        </div>
       </div>
     </div>
   )
